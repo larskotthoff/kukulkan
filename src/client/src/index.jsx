5bb32811768bacf622be9b134c1988687ac0b6e9
Lars Kotthoff <larsko@uwyo.edu>
2022-08-05 16:06:27 -0600

    more theming

--- src/client/src/index.jsx
+++ src/client/src/index.jsx
@@ -192,7 +192,21 @@ class Threads extends React.PureComponent {
   }
 }
 
-export const theme = createTheme({ palette: { mode: 'dark' } });
+export const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#5893df',
+    },
+    secondary: {
+      main: '#2ec5d3',
+    },
+    background: {
+      default: '#192231',
+      paper: '#24344d',
+    }
+  }
+});
 
 function Kukulkan() {
   const [threads, setThreads] = useState(null);
