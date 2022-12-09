d5d974277fe4367f321928b220235a1889234e21
Lars Kotthoff <larsko@uwyo.edu>
2022-12-09 15:25:51 -0700

    reorg

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ rm -rf prod/*
 cd src/client
 npm run build --omit=dev
 cd ../..
-cp src/server/kukulkan.py prod/
+cp src/server/src/kukulkan.py prod/
 mkdir prod/static
 cp -r src/client/build/* prod/static/
 mv prod/static/static/* prod/static/ 
