65b2cc0b43a3c56d9635d6efb47ab27e0fdc0060
Lars Kotthoff <larsko@uwyo.edu>
2022-12-10 13:14:41 -0700

    test accounts and tag add/remove

--- src/server/test/test_client.py
+++ src/server/test/test_client.py
@@ -15,6 +15,15 @@ def setup():
 
     db.close.assert_called_once()
 
+def test_accounts(setup):
+    app, db = setup
+
+    app.config.custom["accounts"] = "foo"
+    with app.test_client() as test_client:
+        response = test_client.get('/api/accounts/')
+        assert response.status_code == 200
+        assert b'"foo"\n' == response.data
+
 def test_tags(setup):
     app, db = setup
 
@@ -35,12 +44,77 @@ def test_raw_message(setup):
     mq = MagicMock()
     mq.search_messages = MagicMock(return_value = iter([mf]))
 
-    with patch("notmuch.Query", return_value = mq):
+    with patch("notmuch.Query", return_value = mq) as q:
         with patch("builtins.open", mock_open(read_data = "This is a test.")):
             with app.test_client() as test_client:
                 response = test_client.get('/api/raw_message/foo')
                 assert response.status_code == 200
                 assert b'This is a test.' == response.data
+        q.assert_called_once_with(db, "id:foo")
 
     mf.get_filename.assert_called_once()
     mq.search_messages.assert_called_once()
+
+def test_tag_add(setup):
+    app, db = setup
+
+    dbw = MagicMock()
+    dbw.close = MagicMock()
+    dbw.begin_atomic = MagicMock()
+    dbw.end_atomic = MagicMock()
+
+    mf = MagicMock()
+    mf.add_tag = MagicMock()
+    mf.tags_to_maildir_flags = MagicMock()
+
+    mq = MagicMock()
+    mq.search_messages = MagicMock(return_value = iter([mf]))
+
+    with patch("notmuch.Database", return_value = dbw):
+        with patch("notmuch.Query", return_value = mq) as q:
+            with app.test_client() as test_client:
+                response = test_client.get('/api/tag/add/message/foo/bar')
+                assert response.status_code == 200
+                assert b'bar' == response.data
+            q.assert_called_once_with(dbw, "id:foo")
+
+    mf.add_tag.assert_called_once()
+    mf.tags_to_maildir_flags.assert_called_once()
+
+    mq.search_messages.assert_called_once()
+
+    dbw.begin_atomic.assert_called_once()
+    dbw.end_atomic.assert_called_once()
+    dbw.close.assert_called_once()
+
+def test_tag_remove(setup):
+    app, db = setup
+
+    dbw = MagicMock()
+    dbw.close = MagicMock()
+    dbw.begin_atomic = MagicMock()
+    dbw.end_atomic = MagicMock()
+
+    mf = MagicMock()
+    mf.remove_tag = MagicMock()
+    mf.tags_to_maildir_flags = MagicMock()
+
+    mq = MagicMock()
+    mq.search_messages = MagicMock(return_value = iter([mf]))
+
+    with patch("notmuch.Database", return_value = dbw):
+        with patch("notmuch.Query", return_value = mq) as q:
+            with app.test_client() as test_client:
+                response = test_client.get('/api/tag/remove/message/foo/bar')
+                assert response.status_code == 200
+                assert b'bar' == response.data
+            q.assert_called_once_with(dbw, "id:foo")
+
+    mf.remove_tag.assert_called_once()
+    mf.tags_to_maildir_flags.assert_called_once()
+
+    mq.search_messages.assert_called_once()
+
+    dbw.begin_atomic.assert_called_once()
+    dbw.end_atomic.assert_called_once()
+    dbw.close.assert_called_once()
