d02d6d63d1bad6474bb7532d1f073904d58a9c9f
Lars Kotthoff <larsko@uwyo.edu>
2023-01-21 14:38:53 -0700

    complete testing of search component

--- src/client/src/search.test.jsx
+++ src/client/src/search.test.jsx
@@ -1,7 +1,6 @@
 import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from '@testing-library/react';
 
-import { unmountComponentAtNode } from "react-dom";
-import { render, screen } from '@testing-library/react';
 import { Search } from './search.jsx';
 
 it('renders', () => {
@@ -9,6 +8,105 @@ it('renders', () => {
   expect(screen.getByRole('combobox', {type: "text"})).toBeInTheDocument();
   expect(document.querySelector(`input[name="search"]`)).toBeInTheDocument();
   expect(document.getElementsByClassName("kukulkan-queryBox")[0]).toBeInTheDocument();
+});
+
+it('respects query property', () => {
+  render(<Search query={"foobar"}/>);
+  expect(screen.getByRole('combobox', {type: "text"})).toBeInTheDocument();
+  expect(document.querySelector(`input[name="search"]`)).toBeInTheDocument();
+  expect(document.querySelector(`input[name="search"]`)).toHaveValue("foobar");
+  expect(document.getElementsByClassName("kukulkan-queryBox")[0]).toBeInTheDocument();
+});
+
+it('has options', () => {
+  render(<Search />);
+  expect(screen.getByRole('combobox', {type: "text"})).toBeInTheDocument();
+  expect(document.querySelector(`input[name="search"]`)).toBeInTheDocument();
+  expect(document.getElementsByClassName("kukulkan-queryBox")[0]).toBeInTheDocument();
+
+  let input = document.querySelector(`input[name="search"]`);
+  input.click();
+  input.focus();
+  fireEvent.keyDown(input, { key: "ArrowDown" });
+  fireEvent.keyDown(input, { key: "Enter" });
+  expect(input.value).toEqual("tag:unread");
+
+  fireEvent.change(input, {target: { value: "" }});
+  fireEvent.keyDown(input, { key: "ArrowDown" });
+  fireEvent.keyDown(input, { key: "ArrowDown" });
+  fireEvent.keyDown(input, { key: "Enter" });
+  expect(input.value).toEqual("tag:todo");
+
+  fireEvent.change(input, {target: { value: "" }});
+  fireEvent.keyDown(input, { key: "ArrowDown" });
+  fireEvent.keyDown(input, { key: "ArrowDown" });
+  fireEvent.keyDown(input, { key: "ArrowDown" });
+  fireEvent.keyDown(input, { key: "Enter" });
+  expect(input.value).toEqual("date:today");
+});
+
+it('gets options from localStorage', () => {
+  const localStorageMock = (function() {
+    return {
+      getItem: function(key) {
+        return '["foo:bar"]';
+      }
+    }
+  })()
+  Object.defineProperty(window, 'localStorage', {
+    value: localStorageMock
+  })
+
+  render(<Search />);
+  expect(screen.getByRole('combobox', {type: "text"})).toBeInTheDocument();
+  expect(document.querySelector(`input[name="search"]`)).toBeInTheDocument();
+  expect(document.getElementsByClassName("kukulkan-queryBox")[0]).toBeInTheDocument();
+
+  let input = document.querySelector(`input[name="search"]`);
+  input.click();
+  input.focus();
+  fireEvent.change(input, {target: { value: "foo" }});
+  fireEvent.keyDown(input, { key: "ArrowDown" });
+  fireEvent.keyDown(input, { key: "Enter" });
+  expect(input.value).toEqual("foo:bar");
+});
+
+it('autocompletes tags', () => {
+  render(<Search allTags={["foobar", "barfoo"]}/>);
+  expect(screen.getByRole('combobox', {type: "text"})).toBeInTheDocument();
+  expect(document.querySelector(`input[name="search"]`)).toBeInTheDocument();
+  expect(document.getElementsByClassName("kukulkan-queryBox")[0]).toBeInTheDocument();
+
+  let input = document.querySelector(`input[name="search"]`);
+  input.click();
+  input.focus();
+  fireEvent.change(input, {target: { value: "tag:foo" }});
+  fireEvent.keyDown(input, { key: "ArrowDown" });
+  fireEvent.keyDown(input, { key: "Enter" });
+  expect(input.value).toEqual("tag:foobar");
+
+  fireEvent.change(input, {target: { value: "tag:foobar and tag:bar" }});
+  fireEvent.keyDown(input, { key: "ArrowDown" });
+  fireEvent.keyDown(input, { key: "Enter" });
+  expect(input.value).toEqual("tag:foobar and tag:barfoo");
+});
+
+it('sets searchParams on submit', () => {
+  const setSearchParams = jest.fn();
+
+  render(<Search setSearchParams={setSearchParams}/>);
+  expect(screen.getByRole('combobox', {type: "text"})).toBeInTheDocument();
+  expect(document.querySelector(`input[name="search"]`)).toBeInTheDocument();
+  expect(document.getElementsByClassName("kukulkan-queryBox")[0]).toBeInTheDocument();
+
+  let input = document.querySelector(`input[name="search"]`);
+  input.click();
+  input.focus();
+  fireEvent.keyDown(input, { key: "ArrowDown" });
+  fireEvent.keyDown(input, { key: "Enter" });
+  expect(input.value).toEqual("tag:unread");
+  fireEvent.submit(input);
+  expect(setSearchParams).toHaveBeenCalledWith({query: "tag:unread"});
 });
 
 // vim: tabstop=2 shiftwidth=2 expandtab
