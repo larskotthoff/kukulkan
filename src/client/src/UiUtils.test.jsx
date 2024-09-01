727ea6c4690ce9a3d5d712633ff78e0c33c0e3e4
Lars Kotthoff <larsko@uwyo.edu>
2024-09-01 10:18:11 -0600

    fix warnings in tests

--- src/client/src/UiUtils.test.jsx
+++ src/client/src/UiUtils.test.jsx
@@ -1,4 +1,5 @@
 import { afterEach, test, expect } from "vitest";
+import { createRoot } from "solid-js";
 import { cleanup, render } from "@solidjs/testing-library";
 import { userEvent } from "@testing-library/user-event";
 
@@ -11,28 +12,43 @@ test("exports", () => {
   expect(simulateKeyPress).not.toBe(undefined);
 });
 
+// ChatGPT helped with the setup for this
 test("mkShortcut works", async () => {
-  render(() => <div/>);
-  let tmp = 0;
-  mkShortcut(["a"], () => tmp += 1);
-  expect(tmp).toBe(0);
-  await userEvent.type(document.body, "a");
-  expect(tmp).toBe(1);
+  await new Promise((resolve) => {
+    createRoot(async (dispose) => {
+      render(() => <div/>);
+      let tmp = 0;
+      mkShortcut(["a"], () => tmp += 1);
+      expect(tmp).toBe(0);
+      await userEvent.type(document.body, "a");
+      expect(tmp).toBe(1);
+
+      dispose();
+      resolve();
+    });
+  });
 });
 
 test("mkShortcut doesn't trigger inside input-likes", async () => {
-  let { container } = render(() => <><input type="text"/><textarea/></>),
-      tmp = 0;
-  mkShortcut(["a"], () => tmp += 1);
-  expect(tmp).toBe(0);
-  await userEvent.type(document.body, "a");
-  expect(tmp).toBe(1);
-  await userEvent.type(container.querySelector("input"), "a");
-  expect(tmp).toBe(1);
-  await userEvent.type(document.body, "a");
-  expect(tmp).toBe(2);
-  await userEvent.type(container.querySelector("textarea"), "a");
-  expect(tmp).toBe(2);
+  await new Promise((resolve) => {
+    createRoot(async (dispose) => {
+      let { container } = render(() => <><input type="text"/><textarea/></>),
+          tmp = 0;
+      mkShortcut(["a"], () => tmp += 1);
+      expect(tmp).toBe(0);
+      await userEvent.type(document.body, "a");
+      expect(tmp).toBe(1);
+      await userEvent.type(container.querySelector("input"), "a");
+      expect(tmp).toBe(1);
+      await userEvent.type(document.body, "a");
+      expect(tmp).toBe(2);
+      await userEvent.type(container.querySelector("textarea"), "a");
+      expect(tmp).toBe(2);
+
+      dispose();
+      resolve();
+    });
+  });
 });
 
 // vim: tabstop=2 shiftwidth=2 expandtab
