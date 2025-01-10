28c096bc6e3116755376f2840ad7561b9cb8cc51
Lars Kotthoff <larsko@uwyo.edu>
2025-01-10 16:28:27 -0700

    test touch events

--- client/src/UiUtils.test.jsx
+++ client/src/UiUtils.test.jsx
@@ -1,11 +1,36 @@
-import { afterEach, test, expect } from "vitest";
+import { afterEach, beforeAll, test, expect } from "vitest";
 import { createRoot } from "solid-js";
-import { cleanup, render } from "@solidjs/testing-library";
+import { cleanup, fireEvent, render } from "@solidjs/testing-library";
 import { userEvent } from "@testing-library/user-event";
 
 afterEach(cleanup);
 
-import { mkShortcut } from "./UiUtils.jsx";
+import { handleSwipe, mkShortcut } from "./UiUtils.jsx";
+
+// claude helped with this
+beforeAll(() => {
+  class Touch {
+    constructor(init) {
+      Object.assign(this, init);
+    }
+  }
+  global.Touch = Touch;
+});
+
+function createTouchEvent(x, y, type = 'touchstart') {
+  const touch = new Touch({
+    identifier: Date.now(),
+    target: document.body,
+    clientX: x,
+    clientY: y
+  });
+
+  return new TouchEvent(type, {
+    cancelable: true,
+    bubbles: true,
+    touches: [touch]
+  });
+}
 
 test("exports", () => {
   expect(mkShortcut).not.toBe(undefined);
@@ -48,6 +73,113 @@ test("mkShortcut doesn't trigger inside input-likes", async () => {
       resolve();
     });
   });
+});
+
+test("handleSwipe works", async () => {
+  let left = 0, right = 0;
+
+  handleSwipe(document.body, () => left += 1, null, () => right += 1, null);
+  expect(left).toBe(0);
+  expect(right).toBe(0);
+
+  let tt = createTouchEvent(0, 0, 'touchstart');
+  // for some reason it doesn't work if this is defined in createTouchEvent
+  tt.touches.item = (i) => tt.touches[i];
+  await fireEvent(document.body, tt);
+  tt = createTouchEvent(100, 0, 'touchmove');
+  tt.touches.item = (i) => tt.touches[i];
+  await fireEvent(document.body, tt);
+  tt = createTouchEvent(100, 0, 'touchend');
+  tt.touches.item = (i) => tt.touches[i];
+  await fireEvent(document.body, tt);
+  expect(left).toBe(0);
+  expect(right).toBe(1);
+
+  tt = createTouchEvent(100, 0, 'touchstart');
+  tt.touches.item = (i) => tt.touches[i];
+  await fireEvent(document.body, tt);
+  tt = createTouchEvent(0, 0, 'touchmove');
+  tt.touches.item = (i) => tt.touches[i];
+  await fireEvent(document.body, tt);
+  tt = createTouchEvent(0, 0, 'touchend');
+  tt.touches.item = (i) => tt.touches[i];
+  await fireEvent(document.body, tt);
+  expect(left).toBe(1);
+  expect(right).toBe(1);
+});
+
+test("handleSwipe doesn't trigger under threshold", async () => {
+  let left = 0, right = 0;
+
+  handleSwipe(document.body, () => left += 1, null, () => right += 1, null);
+  expect(left).toBe(0);
+  expect(right).toBe(0);
+
+  let tt = createTouchEvent(0, 0, 'touchstart');
+  // for some reason it doesn't work if this is defined in createTouchEvent
+  tt.touches.item = (i) => tt.touches[i];
+  await fireEvent(document.body, tt);
+  tt = createTouchEvent(20, 0, 'touchmove');
+  tt.touches.item = (i) => tt.touches[i];
+  await fireEvent(document.body, tt);
+  tt = createTouchEvent(20, 0, 'touchend');
+  tt.touches.item = (i) => tt.touches[i];
+  await fireEvent(document.body, tt);
+  expect(left).toBe(0);
+  expect(right).toBe(0);
+});
+
+test("handleSwipe shows indicators and adjust position of swiped element", async () => {
+  const el = document.createElement("div");
+  el.classList.add("thread");
+
+  handleSwipe(el, () => 1, "leftTest", () => 1, "rightTest");
+
+  // too little movement to adjust element position
+  let tt = createTouchEvent(0, 0, 'touchstart');
+  // for some reason it doesn't work if this is defined in createTouchEvent
+  tt.touches.item = (i) => tt.touches[i];
+  await fireEvent(el, tt);
+  tt = createTouchEvent(10, 0, 'touchmove');
+  tt.touches.item = (i) => tt.touches[i];
+  await fireEvent(el, tt);
+  expect(el.style.left).toBe("");
+  expect(el.children.length).toBe(0);
+
+  // element position adjusted, no trigger shown
+  tt = createTouchEvent(25, 0, 'touchmove');
+  tt.touches.item = (i) => tt.touches[i];
+  await fireEvent(el, tt);
+  expect(el.style.left).toBe("25px");
+  expect(el.children.length).toBe(0);
+
+  tt = createTouchEvent(-25, 0, 'touchmove');
+  tt.touches.item = (i) => tt.touches[i];
+  await fireEvent(el, tt);
+  expect(el.style.left).toBe("-25px");
+  expect(el.children.length).toBe(0);
+
+  // element position adjusted and trigger shown
+  tt = createTouchEvent(75, 0, 'touchmove');
+  tt.touches.item = (i) => tt.touches[i];
+  await fireEvent(el, tt);
+  expect(el.style.left).toBe("75px");
+  expect(el.children.length).toBe(1);
+  expect(el.children[0].innerHTML).toBe("rightTest");
+  tt = createTouchEvent(75, 0, 'touchcancel');
+  await fireEvent(el, tt);
+
+  tt = createTouchEvent(0, 0, 'touchstart');
+  tt.touches.item = (i) => tt.touches[i];
+  await fireEvent(el, tt);
+  tt = createTouchEvent(-75, 0, 'touchmove');
+  tt.touches.item = (i) => tt.touches[i];
+  await fireEvent(el, tt);
+  expect(el.style.left).toBe("-75px");
+  expect(el.children.length).toBe(1);
+  expect(el.children[0].innerHTML).toBe("leftTest");
+  tt = createTouchEvent(-75, 0, 'touchcancel');
+  await fireEvent(el, tt);
 });
 
 // vim: tabstop=2 shiftwidth=2 expandtab
